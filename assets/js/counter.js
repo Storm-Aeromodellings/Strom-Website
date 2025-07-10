2a3c648415b4654911835d47e5a1353fa1953a81
Shashikant Ramteke <stormaeromodellings@gmail.com>
2025-07-11 01:53:44 +0530

    Intro video commit

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,26 +1,46 @@
+function animateHours(id, target) {
+   const el = document.getElementById(id);
+   let count = 0;
+   const speed = 20;
+   const update = () => {
+     if (count < target) {
+       count++;
+       el.innerText = count + '+ Hours';
+       setTimeout(update, speed);
+     }
+   };
+   const observer = new IntersectionObserver(entries => {
+     if (entries[0].isIntersecting) {
+       update();
+       observer.disconnect();
+     }
+   });
+   observer.observe(el);
+ }
 
+ function animateFlights(id, target) {
+   const el = document.getElementById(id);
+   let count = 0;
+   const speed = 20;
+   const update = () => {
+     if (count < target) {
+       count++;
+       el.innerText = count + '+ Flights';
+       setTimeout(update, speed);
+     }
+   };
+   const observer = new IntersectionObserver(entries => {
+     if (entries[0].isIntersecting) {
+       update();
+       observer.disconnect();
+     }
+   });
+   observer.observe(el);
+ }
 
-function animateCounter(id, target) {
-  const el = document.getElementById(id);
-  let count = 0;
-  const speed = 20;
-  const update = () => {
-    if (count < target) {
-      count++;
-      el.innerText = count + '+ Hours';
-      setTimeout(update, speed);
-    }
-  };
-  const observer = new IntersectionObserver(entries => {
-    if (entries[0].isIntersecting) {
-      update();
-      observer.disconnect();
-    }
-  });
-  observer.observe(el);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  animateCounter("manual-hours", 200);
-  animateCounter("auto-hours", 200);
-});
+ document.addEventListener("DOMContentLoaded", () => {
+    animateFlights("manual-count", 1111);
+    animateHours("manual-hours", 273);
+    animateFlights("auto-count", 1479);
+    animateHours("auto-hours", 496);
+ });
